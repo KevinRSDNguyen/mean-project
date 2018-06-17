65d088b916504367b9ea02a43f9708a335a4f496
kevin nguyen <kevinrsdnguyen@gmail.com>
2018-06-16 17:11:20 -0700

    Understanding Cors

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
 app.use("/api/posts", (req, res, next) => {
   const posts = [
-    { id: "sdsf", title: "First server side post", content: "Meow" },
+    { id: "sdsf", title: "First server side post!", content: "Meow" },
     { id: "sdczxcsf", title: "2nd server side post", content: "Moo" }
   ];
   res.status(200).json({
     message: "Posts fetched successfully",
-    posts
+    posts: posts
   });
 });
 
